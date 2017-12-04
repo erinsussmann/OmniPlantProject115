52bd17a0ede468c75f9e68fb7bb5b9fbd7290dfe
Erin <erinsussmann@gmail.com>
2017-12-04 15:57:35 -0500

    Creates Plants, Flowers, Vegetables, and Groundcover Classes

---
+++ Omni Systems Project/FirstPage/src/firstpage/Vegetables.java
@@ -0,0 +1,20 @@
+
+package firstpage;
+
+public class Vegetables extends Plant{
+    public Vegetables(){
+        
+    }
+    
+    public void pepper(){
+        
+    }
+    
+    public void eggplant(){
+        
+    }
+    
+    public void tomato(){
+        
+    }
+}
