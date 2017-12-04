8b99ef2a0c9deee93f29dfe428efd47d0353138e
Erin <erinsussmann@gmail.com>
2017-12-04 15:58:20 -0500

    Creates Plants, Flowers, Vegetables, and Groundcover Classes

---
+++ Omni Systems Project/FirstPage/src/firstpage/Flower.java
@@ -0,0 +1,23 @@
+
+package firstpage;
+
+public class Flower extends Plant {
+    
+    public void Flower(){
+        
+    }
+    
+    public void zinnia(){
+        
+    }
+    
+    public void daisy(){
+        
+    }
+    
+    public void cosmos(){
+        
+    }
+    
+
+}
